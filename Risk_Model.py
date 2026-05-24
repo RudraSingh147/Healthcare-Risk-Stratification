62e853081a9e47a63ffcbafbe584e99dfa8ca87e
Raj Karan Singh <singh1174800@gmail.com>
2026-05-24 14:17:19 +0530

    Add files via upload

---
+++ Risk_Model.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Data Processing
+patient_data = pd.read_csv('patients.csv')
+diagnoses_data = pd.read_csv('diagnoses.csv')
+outcome_data = pd.read_csv('outcomes.csv')
+lab_data = pd.read_csv('labs.csv')
